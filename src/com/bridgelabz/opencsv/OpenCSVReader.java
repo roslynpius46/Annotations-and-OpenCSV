daea4d3ae178dd530cbf45d9abe8c1b6df84d36e
roslynpius <roslyn.pius@gmail.com>
2023-12-17 15:02:04 +0530

    Added: UC 2- Read CSV File all at once

--- src/com/bridgelabz/opencsv/OpenCSVReader.java
+++ src/com/bridgelabz/opencsv/OpenCSVReader.java
@@ -1,33 +1,34 @@
 package com.bridgelabz.opencsv;
 
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
+import com.opencsv.exceptions.CsvException;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
         private static final String SAMPLE_CSV_FILE_PATH = "D:\\GE_BridgeLabz\\Day21_Annotations_and_OpenCSV\\src\\com\\bridgelabz\\opencsv\\sample.csv";
 
-        public static void main(String[] args) throws IOException, CsvValidationException {
+        public static void main(String[] args) throws IOException {
             try(
                     Reader reader= Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
                     CSVReader csvReader=new CSVReader(reader)
             )
             {
-                // Reading Records One by One in a String array
-                String[] nextRecord;
-
-                while((nextRecord = csvReader.readNext())!=null)
-                {
-                    System.out.println("Name:"+nextRecord[0]);
-                    System.out.println("Email:"+nextRecord[1]);
-                    System.out.println("Phone: "+nextRecord[2]);
-                    System.out.println("Country:"+nextRecord[3]);
-                    System.out.println("====================================");
+                // Reading All Records at once into a List<String[]>
+                List<String[]> records = csvReader.readAll();
+                for (String[] record: records) {
+                    System.out.println("Name : " + record[0]);
+                    System.out.println("Email : " + record[1]);
+                    System.out.println("Phone : "+ record[2]);
+                    System.out.println("Country : "+ record[3]);
+                    System.out.println("=================================");
                 }
+        } catch (CsvException e) {
+                throw new RuntimeException(e);
+            }
         }
-        }
-        }
+}
